2b85e3a380176a863650072b218967e6fd814d7c
Guiariston <gui-ariston@outlook.com>
2025-09-05 00:19:45 +0000

    fix: adicionado speedinsights

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import AboutMe from "./components/AboutMe";
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const queryClient = new QueryClient();
 
